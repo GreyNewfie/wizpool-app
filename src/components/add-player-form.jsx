8a3ec8fa8309175fdeb05d1577ea961553f38a1e
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-04-17 23:00:22 -0230

    Update add-player-form
    Update layout and spacing to add player form to make it easier to interact with for the user.
    Add placeholder text to explain what the input text should be by the user.

--- src/components/add-player-form.jsx
+++ src/components/add-player-form.jsx
@@ -6,12 +6,14 @@ export default function AddPlayer() {
         id="player-1-name"
         name="player-1-name"
         className="text-input"
+        placeholder="Player's name"
       />
       <input
         type="text"
         id="player-1-team-name"
         name="player-1-team-name"
         className="text-input"
+        placeholder="Player's team name (optional)"
       />
     </form>
   );
