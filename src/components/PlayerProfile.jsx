580e11f6b601c30ff2f838f381d9d030a1ad3910
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-04-25 22:18:42 -0230

    Create PlayerProfile.jsx
    Moved the PlayerProfile function into it's own component to be used across the app, and simplify ChooseAssignTeamsPlayer page.

---
+++ src/components/PlayerProfile.jsx
@@ -0,0 +1,29 @@
+import PrimaryActionButton from './PrimayActionButton';
+
+export default function PlayerProfile({ player }) {
+  return (
+    <div className="player">
+      <div className="player-info">
+        <img className="team-icon" src={player.icon} alt="team icon" />
+        <div>
+          <h5>{player.name}</h5>
+          <span className="profile-team-name">
+            {player.teamName
+              ? player.teamName
+              : `${player.name}'s Awesome Team`}
+          </span>
+        </div>
+      </div>
+      <PrimaryActionButton
+        props={{
+          text: `Assign Teams to ${player.name}`,
+          handleClick: () => console.log(player.name),
+        }}
+      />
+    </div>
+  );
+}
+
+PlayerProfile.propTypes = {
+  player: PropTypes.object,
+};
