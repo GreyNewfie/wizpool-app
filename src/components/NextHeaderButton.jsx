3f1b6eaa2a785fb6cd01b0db82d9f6367c03c730
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-04-27 19:18:52 -0230

    Add NextHeaderButton.module.css
    Move styles for the next button in to a separate css module.

--- src/components/NextHeaderButton.jsx
+++ src/components/NextHeaderButton.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import classes from './NextHeaderButton.module.css';
 
 export default function NextHeaderButton({ handleClick, path }) {
   return (
     <Link to={path}>
-      <button className="next-header-btn" type="submit" onClick={handleClick}>
+      <button
+        className={classes['next-header-btn']}
+        type="submit"
+        onClick={handleClick}
+      >
         Next
       </button>
     </Link>
