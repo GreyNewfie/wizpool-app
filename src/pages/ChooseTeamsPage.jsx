108f5396578e6fd8648a0e8f41f8ba7838ec9d38
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-02 22:54:16 -0230

    Create ChooseTeamsPage.jsx
    Create ChooseTeamsPage function and add page header element.

---
+++ src/pages/ChooseTeamsPage.jsx
@@ -0,0 +1,7 @@
+import PageHeader from '../components/PageHeader';
+
+export default function ChooseTeamsPage() {
+  return (
+    <PageHeader headerText="Choose Teams" path="/choose-assign-teams-player" />
+  );
+}
