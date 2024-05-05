747cfe601228d5ffe46f9bbd5ae12f911820ba1a
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-05 11:29:31 -0230

    Store selected teams
    Stores the teams selected by the user into state.
    Styles the button for selected teams to show they've been selected.

--- src/components/TeamsList.jsx
+++ src/components/TeamsList.jsx
@@ -1,14 +1,31 @@
 import classes from './TeamsList.module.css';
+import { useState } from 'react';
 import { nflTeams } from '../data';
 
 export default function TeamsList() {
+  const [selectedTeams, setSelectedTeams] = useState([]);
+
+  const toggleSelect = (teamName) => {
+    if (selectedTeams.includes(teamName)) {
+      setSelectedTeams(selectedTeams.filter((team) => team !== teamName));
+    } else {
+      setSelectedTeams([...selectedTeams, teamName]);
+    }
+  };
+
   return (
     <div className={classes['teams-list']}>
       {nflTeams.map((team, index) => {
+        const isSelected = selectedTeams.includes(team.teamName);
         return (
           <div key={index} className={classes['select-team-container']}>
-            <h5>{team.teamName}</h5>
-            <button className={classes['select-btn']}>Select</button>
+            <p>{team.teamName}</p>
+            <button
+              className={`${classes['select-btn']} ${isSelected ? classes['selected'] : ''}`}
+              onClick={() => toggleSelect(team.teamName)}
+            >
+              {isSelected ? 'Selected' : 'Select'}
+            </button>
           </div>
         );
       })}
