d79a9f54d750b7cb1fa86fdaac4fe48b5cc49aa2
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-09 20:43:53 -0230

    Update usePool.js
    Add function to set the initial value of pool to a blank pool or to what is stored in local storage.

--- src/utils/usePool.js
+++ src/utils/usePool.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from 'react';
 import Pool from './Pool';
 
+function getInitialPool() {
+  const storedPool = JSON.parse(localStorage.getItem('pool'));
+  if (storedPool) return storedPool;
+  if (!storedPool) return new Pool('', []);
+}
+
 export default function usePool() {
-  const [pool, setPool] = useState(new Pool('', []));
+  const [pool, setPool] = useState(getInitialPool());
 
   useEffect(() => {
     localStorage.setItem('pool', JSON.stringify(pool));
