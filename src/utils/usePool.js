918aab93999fa1817cd407e3dfa425764e033fe9
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-12 20:14:05 -0230

    API testing
    Create a fetch to the api and return the league information and log it. Includes error logging to the console.

--- src/utils/usePool.js
+++ src/utils/usePool.js
@@ -9,6 +9,8 @@ function getInitialPool() {
 
 export default function usePool() {
   const [pool, setPool] = useState(getInitialPool());
+  const url =
+    'https://api.sportsdata.io/v3/nba/scores/json/Standings/2024?key=b461640f8b2641b8bcaf42396b30ba9a';
 
   useEffect(() => {
     localStorage.setItem('pool', JSON.stringify(pool));
@@ -18,6 +20,23 @@ export default function usePool() {
     const storedPool = JSON.parse(localStorage.getItem('pool'));
     return storedPool;
   }
+
+  const fecthData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (response.status === 200) {
+        console.log(data);
+      } else {
+        console.log('Server Error:', data.message);
+      }
+    } catch (error) {
+      console.log('Fetch Erro:', error);
+    }
+  };
+
+  fecthData();
 
   return {
     pool,
