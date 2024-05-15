d52f9d219d736ddce9adc1ce5b533afe98256947
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-15 20:24:25 -0230

    Remove getStoredPool
    Remove get stored pool from ChoosePlayerList and use the pool current pool object.

--- src/components/ChoosePlayerList.jsx
+++ src/components/ChoosePlayerList.jsx
@@ -2,13 +2,12 @@ import usePool from '../utils/usePool';
 import PlayerProfile from './PlayerProfile';
 
 export default function ChoosePlayerList() {
-  const { getStoredPool } = usePool();
-  const storedPool = getStoredPool();
+  const { pool } = usePool();
 
   return (
     <div className="select-player">
       <h3>Select a player to assign teams</h3>
-      {storedPool.players.map((player, index) => {
+      {pool.players.map((player, index) => {
         return <PlayerProfile key={index} player={player} index={index} />;
       })}
     </div>
