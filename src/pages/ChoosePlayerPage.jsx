2bcb724ffb68ba8f53a3a4ba83802a7aa5ddc0e5
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-20 18:14:59 -0230

    Update ChoosePlayerPage buttons
    Pass in props for left and right buttons. Left button now uses material ui back icon.

--- src/pages/ChoosePlayerPage.jsx
+++ src/pages/ChoosePlayerPage.jsx
@@ -1,11 +1,17 @@
 import PrimaryLinkButton from '../components/PrimaryLinkButton';
 import PageHeader from '../components/PageHeader';
 import ChoosePlayerList from '../components/ChoosePlayerList';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function ChoosePlayerPage() {
   return (
     <div className="assign-teams-page">
-      <PageHeader headerText="Assign Teams" path="/create-pool" />
+      <PageHeader
+        headerText="Assign Teams"
+        path="/create-pool"
+        rightBtnText="Save"
+        leftBtnText={<ArrowBackIcon />}
+      />
       <ChoosePlayerList />
       <PrimaryLinkButton text="Next" />
     </div>
