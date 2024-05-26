4cec15298974298290e5ab9939a2d6cfd0ec416b
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-26 16:30:40 -0230

    Add teams with data to player
    Replace the teamName string with a teams object that contains more information to make it more useful and easier to update from apiData later.

--- src/components/TeamsList.jsx
+++ src/components/TeamsList.jsx
@@ -4,40 +4,43 @@ import useApiData from '../utils/useApiData';
 import SelectTeamSection from './SelectTeamSection';
 import CircularIndeterminate from './Loading';
 
-const updateNbaTeams = (nbaTeams, playerIndex) => {
+const getAvailableNbaTeams = (nbaTeams, playerIndex) => {
   // Get list of players
   const pool = JSON.parse(localStorage.getItem('pool'));
   // Get a list of teams each other player has selected
-  const pickedTeams = pool.players.flatMap((player) => {
+  const selectedTeams = pool.players.flatMap((player) => {
     // If the index matches the current player's index don't include teams
-    if (pool.players.indexOf(player) == playerIndex) {
+    if (pool.players.indexOf(player) == playerIndex || !player.nbaTeams) {
       return [];
     }
     return player.nbaTeams;
   });
   // Remove those teams from the nbaTeamNames list
-  const updatedNbaTeams = nbaTeams.filter(
-    (teamName) => !pickedTeams.includes(teamName),
+  const availableNbaTeams = nbaTeams.filter(
+    (team) =>
+      !selectedTeams.some((selectedTeam) => selectedTeam.name === team.name),
   );
-  return updatedNbaTeams;
+  return availableNbaTeams;
 };
 
 export default function TeamsList({ playerIndex }) {
-  const { getAllNbaTeams, loading } = useApiData();
+  const { getAllNbaTeams, getAllNbaTeamsData, loading } = useApiData();
   const nbaTeams = getAllNbaTeams().sort(); //**** Why is nbaTeams value [] when next line gets executed? ****
+  const nbaTeamsData = getAllNbaTeamsData();
+  const availableNbaTeams = getAvailableNbaTeams(nbaTeamsData, playerIndex);
   // *Q1* Should nbaTeams and updatedNbaTeams be in a useEffect(() => {}, [nbaTeams])
-  const updatedNbaTeams = updateNbaTeams(nbaTeams, playerIndex);
+  const updatedNbaTeams = getAvailableNbaTeams(nbaTeams, playerIndex);
 
   if (loading) return <CircularIndeterminate />;
 
   return (
     <div className={classes['teams-list']}>
-      {updatedNbaTeams.map((teamName, index) => {
+      {availableNbaTeams.map((team, teamIndex) => {
         return (
           <SelectTeamSection
-            key={index}
-            index={index}
-            teamName={teamName}
+            key={teamIndex}
+            team={team}
+            teamName={team}
             playerIndex={playerIndex}
           />
         );
