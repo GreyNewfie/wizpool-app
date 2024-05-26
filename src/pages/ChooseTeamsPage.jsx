8e412405a98709fc7a06ce576f2c7414fbc655bf
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-26 21:27:42 -0230

    Update ChooseTeamsPage.jsx
    Add back arrow back into header using material ui

--- src/pages/ChooseTeamsPage.jsx
+++ src/pages/ChooseTeamsPage.jsx
@@ -3,6 +3,7 @@ import TeamsList from '../components/TeamsList';
 import usePool from '../utils/usePool';
 import classes from './ChooseTeamsPage.module.css';
 import { useParams } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function ChooseTeamsPage() {
   const { id } = useParams();
@@ -10,7 +11,11 @@ export default function ChooseTeamsPage() {
 
   return (
     <div className={classes['choose-teams']}>
-      <PageHeader headerText="Choose Teams" path="/choose-player" />
+      <PageHeader
+        headerText="Choose Teams"
+        path="/choose-player"
+        leftBtnText={<ArrowBackIcon />}
+      />
       <h3>Select Teams for {pool.players[id].playerName}</h3>
       <TeamsList playerIndex={id} />
     </div>
