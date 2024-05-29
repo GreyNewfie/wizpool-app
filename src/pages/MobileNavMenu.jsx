ad117a9672afefe3364ca3e1b035695932794743
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-05-28 22:31:35 -0230

    Update bottom menu styling
    Add hover and active styling to buttons for better user UI.
    Wrap buttons in a link to take the user to the desired component.
    Included p tag in the Link to allow users to also click the text to navigate to the correct component.

--- src/pages/MobileNavMenu.jsx
+++ src/pages/MobileNavMenu.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import classes from './MobileNavMenu.module.css';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import PeopleAltOutlinedIcon from '@mui/icons-material/PeopleAltOutlined';
@@ -9,28 +10,36 @@ export default function MobileNavMenu() {
   return (
     <div className={classes['mobile-nav-menu']}>
       <div className={classes['nav-btn-container']}>
-        <button className={classes['mobile-nav-btn']}>
-          {<HomeOutlinedIcon />}
-        </button>
-        <p>Home</p>
+        <Link to="/pool-home">
+          <button className={classes['mobile-nav-btn']}>
+            <HomeOutlinedIcon />
+          </button>
+          <p>Home</p>
+        </Link>
       </div>
       <div className={classes['nav-btn-container']}>
-        <button className={classes['mobile-nav-btn']}>
-          {<PeopleAltOutlinedIcon />}
-        </button>
-        <p>Players</p>
+        <Link>
+          <button className={classes['mobile-nav-btn']}>
+            <PeopleAltOutlinedIcon />
+          </button>
+          <p>Players</p>
+        </Link>
       </div>
       <div className={classes['nav-btn-container']}>
-        <button className={classes['mobile-nav-btn']}>
-          {<CheckCircleOutlinedIcon />}
-        </button>
-        <p>Picks</p>
+        <Link>
+          <button className={classes['mobile-nav-btn']}>
+            <CheckCircleOutlinedIcon />
+          </button>
+          <p>Picks</p>
+        </Link>
       </div>
       <div className={classes['nav-btn-container']}>
-        <button className={classes['mobile-nav-btn']}>
-          {<SettingsOutlinedIcon />}
-        </button>
-        <p>Settings</p>
+        <Link>
+          <button className={classes['mobile-nav-btn']}>
+            <SettingsOutlinedIcon />
+          </button>
+          <p>Settings</p>
+        </Link>
       </div>
     </div>
   );
