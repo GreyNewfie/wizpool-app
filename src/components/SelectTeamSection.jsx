eddcee9d75a543a7b794543b503d37f65bfd0da2
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-02 11:25:55 -0230

    Fix toggleSelect in SelectTeamSection

--- src/components/SelectTeamSection.jsx
+++ src/components/SelectTeamSection.jsx
@@ -45,7 +45,9 @@ export default function SelectTeamSection(props) {
       // Update the isSelected value to true to udpate the button
       setIsSelected(true);
     }
+    console.log('Pool before team added: ', pool);
     setPool(updatedPool);
+    console.log('Pool after team added: ', pool);
   };
 
   return (
