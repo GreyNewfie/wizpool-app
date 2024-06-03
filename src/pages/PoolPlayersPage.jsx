f5f290799844fe43e3cd6a7c43fc6f63107d41e9
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-02 22:05:38 -0230

    Update PoolPlayersPage
    Adds player profiles and bottom menu to the page.
    Updates layout for the overall page.

--- src/pages/PoolPlayersPage.jsx
+++ src/pages/PoolPlayersPage.jsx
@@ -1,15 +1,35 @@
 import PageHeader from '../components/PageHeader';
+import PlayerHomeProfile from '../components/PlayerHomeProfile';
+import MobileNavMenu from './MobileNavMenu';
+import PrimaryActionButton from '../components/PrimayActionButton';
+import usePool from '../utils/usePool';
 import classes from './PoolPlayersPage.module.css';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 export default function PoolPlayersPage() {
+  const { pool } = usePool();
+
   return (
-    <div className={classes['players-teams-container']}>
+    <div className={classes['pool-players']}>
       <PageHeader
         headerText="Player's Teams"
         path="/pool-home"
         leftBtnText=<ArrowBackIcon />
       />
+      <div className={classes['players-container']}>
+        {pool.players.map((player, index) => {
+          return (
+            <div key={index} className="player">
+              <PlayerHomeProfile
+                key={index}
+                player={player}
+                playerIndex={index}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
 }
