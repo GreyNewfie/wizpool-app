11e6259f72c04ee0a2c0e40385a51619c9311f96
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-07 08:13:03 -0230

    Update styling of PlayerTeams component
    Add spacing, text styling and icon background for a better ux

--- src/pages/PoolPlayersPage.jsx
+++ src/pages/PoolPlayersPage.jsx
@@ -4,6 +4,7 @@ import MobileNavMenu from './MobileNavMenu';
 import usePool from '../utils/usePool';
 import classes from './PoolPlayersPage.module.css';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import PlayerTeams from '../components/PlayerTeams';
 
 export default function PoolPlayersPage() {
   const { pool } = usePool();
@@ -18,14 +19,19 @@ export default function PoolPlayersPage() {
       <div className={classes['players-container']}>
         {pool.players.map((player, index) => {
           return (
-            <div key={index} className={classes['player']}>
-              <PlayerHomeProfile
-                key={index}
-                player={player}
-                playerIndex={index}
-              />
-              <button className={classes['view-teams-btn']}>View Teams</button>
-            </div>
+            <>
+              <div key={index} className={classes['player']}>
+                <PlayerHomeProfile
+                  key={index}
+                  player={player}
+                  playerIndex={index}
+                />
+                <button className={classes['view-teams-btn']}>
+                  View Teams
+                </button>
+              </div>
+              <PlayerTeams player={player} />
+            </>
           );
         })}
       </div>
