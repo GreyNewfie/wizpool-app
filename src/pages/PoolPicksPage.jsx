8f18646e4d83dc1c4a24cab59ffbff3c8cc473b5
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-12 22:42:23 -0230

    Update PoolPicksPage to display picks
    Display each team with the team profile information and the player that picked that team.

--- src/pages/PoolPicksPage.jsx
+++ src/pages/PoolPicksPage.jsx
@@ -2,8 +2,27 @@ import classes from './PoolPicksPage.module.css';
 import PageHeader from '../components/PageHeader';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import MobileNavMenu from './MobileNavMenu';
+import usePool from '../utils/usePool';
+import DisplayTeams from '../components/DisplayTeams';
 
 export default function PoolPicksPage() {
+  const { pool } = usePool();
+
+  const getTeamsAndPlayers = () => {
+    const clonedPool = pool.clonePool();
+    const TeamsWithPlayers = clonedPool.players.flatMap((player) => {
+      return player.nbaTeams.map((team) => {
+        return {
+          playerName: player.playerName,
+          team: team,
+        };
+      });
+    });
+    return TeamsWithPlayers;
+  };
+
+  const teamsAndPlayersList = getTeamsAndPlayers();
+
   return (
     <div className={classes['pool-picks']}>
       <PageHeader
@@ -11,7 +30,14 @@ export default function PoolPicksPage() {
         leftBtnText=<ArrowBackIcon />
         path="/pool-home"
       />
-      <div className={classes['picks-container']}></div>
+      <div className={classes['picks-container']}>
+        {teamsAndPlayersList.map((teamAndPlayer, index) => (
+          <div key={index} className={classes['player-team-container']}>
+            <DisplayTeams teams={[teamAndPlayer.team]} />
+            <h5>{teamAndPlayer.playerName}</h5>
+          </div>
+        ))}
+      </div>
       <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
