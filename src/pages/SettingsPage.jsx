0971701118b53390ed15d3db5a83c14166f62cbb
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-14 16:38:45 -0230

    Update SettingsPage options
    Add league name and setting to modify players.

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -2,8 +2,13 @@ import classes from './SettingsPage.module.css';
 import PageHeader from '../components/PageHeader';
 import MobileNavMenu from './MobileNavMenu';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import usePool from '../utils/usePool';
+import { Link } from 'react-router-dom';
 
 export default function SettingsPage() {
+  const { pool } = usePool();
+
   return (
     <div className={classes['pool-settings']}>
       <PageHeader
@@ -11,6 +16,24 @@ export default function SettingsPage() {
         leftBtnText=<ArrowBackIcon />
         path="/pool-home"
       />
+      <div className={classes['settings-content']}>
+        <div className={classes['league-setting']}>
+          <h4>League</h4>
+          <div className={classes['league-info']}>
+            <p>League Name</p>
+            <p>{pool.poolName}</p>
+          </div>
+        </div>
+        <div className={classes['manage-players']}>
+          <h4>Manage Players</h4>
+          <div className={classes['modify-players']}>
+            <Link to="/pool-home">
+              <p>Add, remove or modify players</p>
+              <KeyboardArrowRightIcon />
+            </Link>
+          </div>
+        </div>
+      </div>
       <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
