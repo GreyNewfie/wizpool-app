604b6843c030838b63651ba07d1496112a002465
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-18 22:18:22 -0230

    Create ManagePlayersPage.jsx
    Create the initial ManagePlayersPage component with header and footer.

---
+++ src/pages/ManagePlayersPage.jsx
@@ -0,0 +1,18 @@
+import classes from './ManagePlayersPage.module.css';
+import PageHeader from '../components/PageHeader';
+import MobileNavMenu from './MobileNavMenu';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export default function ManageTeamsPage() {
+  return (
+    <div className={classes['manage-teams']}>
+      <PageHeader
+        headerText="Manage Players"
+        leftBtnText=<ArrowBackIcon />
+        path="/pool-home"
+      />
+      <div className={classes['players-container']}></div>
+      <MobileNavMenu className={classes['bottom-menu']} />
+    </div>
+  );
+}
