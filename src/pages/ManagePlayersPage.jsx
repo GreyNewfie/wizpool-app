8c2ae505e5487230c218769fbd2db9d7dd3e357e
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-20 21:37:21 -0230

    Update ManagePlayersPage with players
    Add player profiles to the page

--- src/pages/ManagePlayersPage.jsx
+++ src/pages/ManagePlayersPage.jsx
@@ -2,16 +2,32 @@ import classes from './ManagePlayersPage.module.css';
 import PageHeader from '../components/PageHeader';
 import MobileNavMenu from './MobileNavMenu';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import PlayerHomeProfile from '../components/PlayerHomeProfile';
+import usePool from '../utils/usePool';
 
 export default function ManageTeamsPage() {
+  const {pool} = usePool();
+
   return (
-    <div className={classes['manage-teams']}>
+    <div className={classes['manage-players']}>
       <PageHeader
         headerText="Manage Players"
         leftBtnText=<ArrowBackIcon />
         path="/pool-settings"
       />
-      <div className={classes['players-container']}></div>
+      <div className={classes['players-container']}>
+        {
+          pool.players.map((player, index) => (
+            <div key={index} className={classes['player']}>
+            <PlayerHomeProfile
+              player={player}
+              playerIndex={index}
+            />
+          </div>
+          )
+          )
+        }
+      </div>
       <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
