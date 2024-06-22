a780ab40ad6fd5b93fe4be33a4bc461b7b731f0e
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-22 18:29:24 -0230

    Create EditPlayerPage
    Add EditPlayerPage with header and footer, and basic page layout.

---
+++ src/pages/EditPlayerPage.jsx
@@ -0,0 +1,22 @@
+import classes from './EditPlayerPage.module.css';
+import PropTypes from 'prop-types';
+import PageHeader from '../components/PageHeader';
+import MobileNavMenu from './MobileNavMenu';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+export default function EditPlayerPage({ player }) {
+  return (
+    <div className={classes['edit-player-container']}>
+      <PageHeader
+        headerText="Edit Player"
+        leftBtnText=<ArrowBackIcon />
+        path="/pool-settings"
+      />
+      <MobileNavMenu className={classes['bottom-menu']} />
+    </div>
+  );
+}
+
+EditPlayerPage.propTypes = {
+  player: PropTypes.object,
+};
