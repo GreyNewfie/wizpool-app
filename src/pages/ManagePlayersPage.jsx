2cf66f4f46559f75fee01fc3942b2fc8198695e6
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-06-25 22:56:25 -0230

    Update editing players on ManagePlayersPage
    Display inputs to allow users to edit player's name and team name on the same page to simplify the UI.

--- src/pages/ManagePlayersPage.jsx
+++ src/pages/ManagePlayersPage.jsx
@@ -4,10 +4,16 @@ import MobileNavMenu from './MobileNavMenu';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import PlayerHomeProfile from '../components/PlayerHomeProfile';
 import usePool from '../utils/usePool';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import PlayerInput from '../components/PlayerInput';
 
 export default function ManageTeamsPage() {
-  const {pool} = usePool();
+  const { pool } = usePool();
+  const [playerToEdit, setPlayerToEdit] = useState(null);
+
+  const togglePlayerToEdit = (index) => {
+    setPlayerToEdit((prevIndex) => (prevIndex === index ? null : index));
+  };
 
   return (
     <div className={classes['manage-players']}>
@@ -17,25 +23,20 @@ export default function ManageTeamsPage() {
         path="/pool-settings"
       />
       <div className={classes['players-container']}>
-        {
-          pool.players.map((player, index) => (
-            <div key={index} className={classes['player']}>
-            <PlayerHomeProfile
-              player={player}
-              playerIndex={index}
-            />
-            <Link to='/edit-player'>            
-              <button 
-                className={classes['edit-btn']}
-                onClick={console.log("Edit player ${player.name}")}
-                >
-                  Edit
-                </button>
-            </Link>
+        {pool.players.map((player, index) => (
+          <div key={index} className={classes['player']}>
+            {playerToEdit !== index && (
+              <PlayerHomeProfile player={player} playerIndex={index} />
+            )}
+            {playerToEdit === index && <PlayerInput player={player} />}
+            <button
+              className={classes['edit-btn']}
+              onClick={() => togglePlayerToEdit(index)}
+            >
+              {playerToEdit === index ? 'Save' : 'Edit'}
+            </button>
           </div>
-          )
-          )
-        }
+        ))}
       </div>
       <MobileNavMenu className={classes['bottom-menu']} />
     </div>
