ee31963ab8ba80edfe5d42435ff5141a70b7879f
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-02 15:55:24 -0230

    Update assign teams page with player profiles
    Add profile for each player along with a button to choose which player the user would like to change the teams for.

--- src/pages/ReassignTeamsPage.jsx
+++ src/pages/ReassignTeamsPage.jsx
@@ -2,8 +2,18 @@ import classes from './ReassignTeamsPage.module.css';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import PageHeader from '../components/PageHeader';
 import MobileNavMenu from './MobileNavMenu';
+import PlayerHomeProfile from '../components/PlayerHomeProfile';
+import usePool from '../utils/usePool';
+import { useState } from 'react';
 
 export default function ReassignTeamsPage() {
+  const { pool, setPool } = usePool();
+  const [playerToEdit, setPlayerToEdit] = useState(null);
+
+  const togglePlayerToEdit = (index) => {
+    setPlayerToEdit((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
     <div className={classes['reassign-teams']}>
       <PageHeader
@@ -11,7 +21,18 @@ export default function ReassignTeamsPage() {
         leftBtnText=<ArrowBackIcon />
         path="/pool-settings"
       />
-      <div className={classes['players-container']}></div>
+      <div className={classes['players-container']}>
+        {pool.players.map((player, index) => {
+          return (
+            <div key={index} className={classes['player']}>
+              <PlayerHomeProfile player={player} playerIndex={index} />
+              {playerToEdit !== index && (
+                <button className={classes['edit-btn']}>Edit</button>
+              )}
+            </div>
+          );
+        })}
+      </div>
       <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
