7d983cca6c3a7aa521a123700ef6a12a84e8f41d
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-03 15:44:11 -0230

    Update CreatePoolPage change handling
    Separate updating state and input value as the user inputs player names and team names. This allows for debounce to be utilized to minimize state updates, while allowing the input value to be updated immediately.

--- src/components/UserTextInput.jsx
+++ src/components/UserTextInput.jsx
@@ -1,22 +1,43 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import classes from './UserTextInput.module.css';
+import { useCallback, useState } from 'react';
 
 export default function UserTextInput(props) {
+  // localValue being used to show input value
+  const [localValue, setLocalValue] = useState(props.value);
   const inputClassName = classNames(
     classes['user-text-input'],
     props.className,
   );
+
+  function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => func(...args), timeout);
+    };
+  }
+
+  // Using debounce on handleChange to delay setting state as user enters input value
+  const debounceHandleChange = useCallback(debounce(props.handleChange), [
+    props.handleChange,
+  ]);
+
+  const handleChange = (e) => {
+    setLocalValue(e.target.value);
+    debounceHandleChange(e);
+  };
 
   return (
     <input
       type="text"
       id={props.id}
       name={props.name}
-      value={props.value}
+      value={localValue}
       className={inputClassName}
       placeholder={props.placeholderText}
-      onChange={props.handleChange}
+      onChange={handleChange}
     />
   );
 }
@@ -26,6 +47,6 @@ UserTextInput.propTypes = {
   placeholderText: PropTypes.string,
   name: PropTypes.string,
   value: PropTypes.string,
-  handleChange: PropTypes.func,
+  handleChange: PropTypes.func.isRequired,
   className: PropTypes.string,
 };
