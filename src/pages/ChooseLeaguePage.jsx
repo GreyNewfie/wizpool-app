519b483c5d41b67f351483d0ae91f84b4aecdc07
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-11 12:06:54 -0230

    Create ChooseLeaguePage component
    Add initial ChooseLeaguePage component with basic styling that will allow the user to choose a league for their pool.
    Update main.jsx with route to the page.

---
+++ src/pages/ChooseLeaguePage.jsx
@@ -0,0 +1,20 @@
+import classes from './ChooseLeaguePage.module.css';
+import NextHeaderButton from '../components/NextHeaderButton';
+
+export default function ChooseLeaugePage() {
+  return (
+    <div
+      id="choose-league-container"
+      className={classes['choose-league-container']}
+    >
+      <div className={classes['choose-league-page-header']}>
+        <NextHeaderButton path="/create-pool" />
+        <h1>Choose a League</h1>
+      </div>
+
+      <div className={classes['instructions-container']}>
+        <p>Choose MLB, NBA or NFL to begin creating your pool</p>
+      </div>
+    </div>
+  );
+}
