fddbbe2bd424badde7d3bc5afb892942b6fdd94d
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-15 15:54:08 -0230

    Update useApiData with multiple leagues
    Check which league is associated with the pool and return the data for that league

--- src/utils/useApiData.js
+++ src/utils/useApiData.js
@@ -4,9 +4,14 @@ export default function useApiData() {
   const [apiData, setApiData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const league = JSON.parse(localStorage.getItem('pool')).league;
 
   const nbaStandingsUrl =
     'https://api.sportsdata.io/v3/nba/scores/json/Standings/2024?key=b461640f8b2641b8bcaf42396b30ba9a';
+  const mlbStandingsUrl =
+    'https://api.sportsdata.io/v3/mlb/scores/json/Standings/2024?key=52a40f632efb4cb5820c9dd879fbdd0d';
+  const nflStandingsUrl =
+    'https://api.sportsdata.io/v3/nfl/scores/json/Standings/2023?key=e51892e63199402da350f44a963a7a81';
 
   const fecthData = async () => {
     setLoading(true);
@@ -22,14 +27,29 @@ export default function useApiData() {
         storedData &&
         storedData.storedDate.day === currentDay &&
         storedData.storedDate.month === currentMonth &&
-        storedData.storedDate.year === currentYear
+        storedData.storedDate.year === currentYear &&
+        storedData.sportsLeague === league
       ) {
         // if yes, use data from localStorage
         data = storedData.data;
-        console.log('Using nbaData from localStorage');
+        console.log('Using data from localStorage');
       } else {
+        let url;
+        switch (league) {
+          case 'nba':
+            url = nbaStandingsUrl;
+            break;
+          case 'mlb':
+            url = mlbStandingsUrl;
+            break;
+          case 'nfl':
+            url = nflStandingsUrl;
+            break;
+          default:
+            throw new Error('No pool league found');
+        }
         // if no, fetch data from API and put it in localStorage
-        const response = await fetch(nbaStandingsUrl);
+        const response = await fetch(url);
         console.log('API called from fetchData');
 
         if (response.status === 200) {
@@ -42,6 +62,7 @@ export default function useApiData() {
               month: currentMonth,
               year: currentYear,
             },
+            sportsLeague: league,
           };
           localStorage.setItem('storedData', JSON.stringify(dataToStore));
         } else {
@@ -71,15 +92,34 @@ export default function useApiData() {
   };
 
   const getAllTeamsData = (apiData) => {
+    function splitTeamAndCity(fullTeamName) {
+      const words = fullTeamName.split(' ');
+      const teamName = words.pop();
+      const city = words.join(' ');
+      return { city, teamName };
+    }
     const allTeamsData = apiData?.map((team) => {
-      const teamData = {
-        teamId: team.Key,
-        city: team.City,
-        name: team.Name,
-        wins: team.Wins,
-        losses: team.Losses,
-        division: team.Division,
-      };
+      let teamData;
+      if (league === 'nfl') {
+        const cityAndTeamName = splitTeamAndCity(team.Name);
+        teamData = {
+          teamId: team.team,
+          city: cityAndTeamName.city,
+          name: cityAndTeamName.teamName,
+          wins: team.Wins,
+          losses: team.Losses,
+          division: `${team.Conference} ${team.Division}`,
+        };
+      } else {
+        teamData = {
+          teamId: team.Key,
+          city: team.City,
+          name: team.Name,
+          wins: team.Wins,
+          losses: team.Losses,
+          division: team.Division,
+        };
+      }
       return teamData;
     });
     return allTeamsData;
