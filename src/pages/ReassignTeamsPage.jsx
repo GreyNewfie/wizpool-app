99f4f6af0f4efb3b6d2cf0acc108942cb1054e63
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-17 15:36:11 -0230

    Update settings page with desktop header
    Add desktop navigation header depending on the size of the user's screen to the ManagePlayersPage and ReassignTeamsPage components.

--- src/pages/ReassignTeamsPage.jsx
+++ src/pages/ReassignTeamsPage.jsx
@@ -6,44 +6,54 @@ import PlayerHomeProfile from '../components/PlayerHomeProfile';
 import TeamsList from '../components/TeamsList';
 import usePool from '../utils/usePool';
 import { Fragment, useState } from 'react';
+import useIsDesktop from '../utils/useIsDesktop';
+import DesktopNavHeader from '../components/DesktopNavHeader';
 
 export default function ReassignTeamsPage() {
   const { pool, setPool } = usePool();
   const [playerToEdit, setPlayerToEdit] = useState(null);
+  const isDesktop = useIsDesktop();
 
   const togglePlayerToEdit = (index) => {
     setPlayerToEdit((prevIndex) => (prevIndex === index ? null : index));
   };
 
   return (
-    <div className={classes['reassign-teams']}>
-      <PageHeader
-        headerText="Rassign Teams"
-        leftBtnText=<ArrowBackIcon />
-        path="/pool-settings"
-      />
-      <div className={classes['players-container']}>
-        <p>{`Select edit to begin reassigning a player's teams`}</p>
-        {pool.players.map((player, index) => {
-          return (
-            <Fragment key={index}>
-              <div key={index} className={classes['player']}>
-                <PlayerHomeProfile player={player} playerIndex={index} />
-                <button
-                  className={classes['edit-btn']}
-                  onClick={() => togglePlayerToEdit(index)}
-                >
-                  {playerToEdit === index ? 'Save' : 'Edit'}
-                </button>
-              </div>
-              {playerToEdit === index && (
-                <TeamsList pool={pool} setPool={setPool} playerIndex={index} />
-              )}
-            </Fragment>
-          );
-        })}
+    <div className={classes['page-container']}>
+      {isDesktop && <DesktopNavHeader />}
+      <div className={classes['reassign-teams']}>
+        <PageHeader
+          headerText="Rassign Teams"
+          leftBtnText=<ArrowBackIcon />
+          path="/pool-settings"
+        />
+        <div className={classes['players-container']}>
+          <p>{`Select edit to begin reassigning a player's teams`}</p>
+          {pool.players.map((player, index) => {
+            return (
+              <Fragment key={index}>
+                <div key={index} className={classes['player']}>
+                  <PlayerHomeProfile player={player} playerIndex={index} />
+                  <button
+                    className={classes['edit-btn']}
+                    onClick={() => togglePlayerToEdit(index)}
+                  >
+                    {playerToEdit === index ? 'Save' : 'Edit'}
+                  </button>
+                </div>
+                {playerToEdit === index && (
+                  <TeamsList
+                    pool={pool}
+                    setPool={setPool}
+                    playerIndex={index}
+                  />
+                )}
+              </Fragment>
+            );
+          })}
+        </div>
+        {!isDesktop && <MobileNavMenu className={classes['bottom-menu']} />}{' '}
       </div>
-      <MobileNavMenu className={classes['bottom-menu']} />
     </div>
   );
 }
