8467f4bd217c77e5216981f3d517915a393186fe
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-23 20:46:44 -0230

    Remove debounce from UserTextInput.js

--- src/components/UserTextInput.jsx
+++ src/components/UserTextInput.jsx
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import classes from './UserTextInput.module.css';
 import { useCallback, useState } from 'react';
+import debounce from '../utils/debounce';
 
 export default function UserTextInput(props) {
   // localValue being used to show input value immediately
@@ -10,14 +11,6 @@ export default function UserTextInput(props) {
     classes['user-text-input'],
     props.className,
   );
-
-  function debounce(func, timeout = 400) {
-    let timer;
-    return (...args) => {
-      clearTimeout(timer);
-      timer = setTimeout(() => func(...args), timeout);
-    };
-  }
 
   // Using debounce on handleChange to delay updating state as user enters input value
   const debounceHandleChange = useCallback(debounce(props.handleChange), [
