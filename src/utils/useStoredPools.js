96e04bba31719f1049d78e1221f41c66b5991c71
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-23 21:57:21 -0230

    Add commented questions
    Questions to discuss in my next coaching session.

--- src/utils/useStoredPools.js
+++ src/utils/useStoredPools.js
@@ -13,6 +13,7 @@ export default function useStoredPools() {
           const poolData = localStorage.getItem(key);
           try {
             const pool = JSON.parse(poolData);
+            // Q1: Should this be new Pool(pool.poolName, pool.players, pool.league, pool.id)?
             pools.push(pool);
           } catch (e) {
             console.error(`Error parsing data for key ${key}:`, e);
@@ -26,6 +27,8 @@ export default function useStoredPools() {
   }, []);
 
   const getNonActivePools = () => {
+    // Q2: activePoolId is a state variable in usePool, should this be using that state variable?
+    // Q3: How would using a state variable from usePool impact this hook?
     const activePoolId = localStorage.getItem('activePoolId');
     const nonActivePools = pools.filter((pool) => pool.id !== activePoolId);
     return nonActivePools;
