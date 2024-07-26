59ecd5537f670fdd7200b7d68a0d3c4566b85011
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-26 13:04:29 -0230

    Add CSS module for ChoosePlayerList
    Move styling into CSS module to make it consistent with other components.

--- src/components/ChoosePlayerList.jsx
+++ src/components/ChoosePlayerList.jsx
@@ -1,11 +1,12 @@
 import usePool from '../utils/usePool';
 import PlayerProfile from './PlayerProfile';
+import classes from './ChoosePlayerList.module.css';
 
 export default function ChoosePlayerList() {
   const { pool } = usePool();
 
   return (
-    <div className="select-player">
+    <div className={classes['select-player']}>
       <h3>Select a player to assign teams</h3>
       {pool.players.map((player, index) => {
         return <PlayerProfile key={index} player={player} index={index} />;
