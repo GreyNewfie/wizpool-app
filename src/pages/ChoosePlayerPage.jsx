7b746b55f05685f17e711164197159115956b506
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-26 14:28:05 -0230

    Add disabling ChooosePlayerPage next button
    Next button is disabled until all players have been assigned teams.

--- src/pages/ChoosePlayerPage.jsx
+++ src/pages/ChoosePlayerPage.jsx
@@ -3,16 +3,26 @@ import BackHeaderButton from '../components/BackHeaderButton';
 import NextHeaderButton from '../components/NextHeaderButton';
 import classes from './ChoosePlayerPage.module.css';
 import usePool from '../utils/usePool';
+import { useEffect, useState } from 'react';
 
 export default function ChoosePlayerPage() {
   const { pool } = usePool();
+  const [areTeamsSelected, setAreTeamsSelected] = useState(false);
+
+  useEffect(() => {
+    const playersHaveTeams = () => {
+      return pool.players.every((player) => player.teams?.length > 0);
+    };
+
+    setAreTeamsSelected(playersHaveTeams());
+  }, [pool.players]);
 
   return (
     <div className={classes['assign-teams-page']}>
       <div className={classes['assign-teams-page-header']}>
         <BackHeaderButton path="/create-pool" />
         <h2>Assign Teams</h2>
-        <NextHeaderButton path="/choose-player" />
+        <NextHeaderButton path="/pool-page" disabled={!areTeamsSelected} />
       </div>
       <ChoosePlayerList poolPlayers={pool.players} />
     </div>
