76c7513473de52394fb5c39f64aafe694d0440f2
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-07-30 18:09:06 -0230

    Create DoneHeaderButton component
    Create a button for user to select when they've finished choosing the teams to assign to the chosen player.

---
+++ src/components/DoneHeaderButton.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import classes from './DoneHeaderButton.module.css';
+
+export default function DoneHeaderButton({ path }) {
+  const navigate = useNavigate();
+
+  const handleNextClick = () => {
+    navigate(path);
+  };
+
+  return (
+    <button
+      className={classes['done-header-btn']}
+      type="submit"
+      onClick={handleNextClick}
+    >
+      Next
+    </button>
+  );
+}
+
+DoneHeaderButton.propTypes = {
+  path: PropTypes.string,
+};
