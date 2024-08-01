b3f2d88740ce6e8f8c668fd95fe87258404b21b4
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-08-01 12:32:52 -0230

    Add basename to router

--- src/main.jsx
+++ src/main.jsx
@@ -17,68 +17,71 @@ import ThemeProvider from './context/ThemeContext.jsx';
 import ChooseLeaguePage from './pages/ChooseLeaguePage.jsx';
 import DeletePoolPage from './pages/DeletePoolPage.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/choose-league',
-    element: <ChooseLeaguePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/create-pool',
-    element: <CreatePoolPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/choose-player',
-    element: <ChoosePlayerPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/choose-teams/:id',
-    element: <ChooseTeamsPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/pool-home',
-    element: <PoolHomePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/pool-players',
-    element: <PoolPlayersPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/pool-picks',
-    element: <PoolPicksPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/pool-settings',
-    element: <SettingsPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/manage-players',
-    element: <ManagePlayersPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/reassign-teams',
-    element: <ReassignTeamsPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: '/delete-pool',
-    element: <DeletePoolPage />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/choose-league',
+      element: <ChooseLeaguePage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/create-pool',
+      element: <CreatePoolPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/choose-player',
+      element: <ChoosePlayerPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/choose-teams/:id',
+      element: <ChooseTeamsPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/pool-home',
+      element: <PoolHomePage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/pool-players',
+      element: <PoolPlayersPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/pool-picks',
+      element: <PoolPicksPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/pool-settings',
+      element: <SettingsPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/manage-players',
+      element: <ManagePlayersPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/reassign-teams',
+      element: <ReassignTeamsPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: '/delete-pool',
+      element: <DeletePoolPage />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: '/wizpool-app/' },
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
