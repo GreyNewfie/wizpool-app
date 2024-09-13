66f352f2eb087a90c5300d1a4ff30b8e729743b7
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-09-13 16:39:52 -0230

    Update PlayerHomeProfile avatar bg
    Account for tied standings when assigning player's avatar background color.

--- src/components/PlayerHomeProfile.jsx
+++ src/components/PlayerHomeProfile.jsx
@@ -7,11 +7,12 @@ export default function PlayerHomeProfile(props) {
     <div className={classes['player-home-profile']}>
       <img
         className={
-          props.playerStanding === '1st'
+          props.playerStanding === '1st' || props.playerStanding === 'T-1st'
             ? classes['first-place-icon']
-            : props.playerStanding === '2nd'
+            : props.playerStanding === '2nd' || props.playerStanding === 'T-2nd'
               ? classes['second-place-icon']
-              : props.playerStanding === '3rd'
+              : props.playerStanding === '3rd' ||
+                  props.playerStanding === 'T-3rd'
                 ? classes['third-place-icon']
                 : classes['team-icon']
         }
