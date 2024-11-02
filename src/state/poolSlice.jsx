3a222adb163b22aa6604d28ea1cc36d1bf419bc0
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-11-02 17:34:53 -0230

    Add setPoolName and AddPlayer to poolSlice
    Add reducers setPoolName and addPlayer to handle changing the pool name and add players to the pool.

--- src/state/poolSlice.jsx
+++ src/state/poolSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
 
 const initialState = {
   id: '',
@@ -17,9 +18,15 @@ const poolSlice = createSlice({
     setLeague: (state, action) => {
       return { ...state, league: action.payload };
     },
+    setPoolName: (state, action) => {
+      return { ...state, name: action.payload };
+    },
+    addPlayer: (state, action) => {
+      state.players.push({ ...action.payload, id: uuid() });
+    },
   },
 });
 
-export const { setPool, setLeague } = poolSlice.actions;
+export const { setPool, setLeague, setPoolName, addPlayer } = poolSlice.actions;
 
 export default poolSlice.reducer;
