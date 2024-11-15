1d71fc1baa713f7f57c288fcbc88281282aca4e3
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-11-15 15:30:15 -0330

    Install and setup Clerk
    Install the Clerk package, and make Clerk user authorization status available throughout the app.

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,13 @@ import ChooseLeaguePage from './pages/ChooseLeaguePage.jsx';
 import DeletePoolPage from './pages/DeletePoolPage.jsx';
 import { Provider } from 'react-redux';
 import store from './state/store';
+import { ClerkProvider } from '@clerk/clerk-react';
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
 
 const router = createBrowserRouter(
   [
@@ -87,10 +94,12 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider>
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </Provider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <Provider store={store}>
+        <ThemeProvider>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </Provider>
+    </ClerkProvider>
   </React.StrictMode>,
 );
