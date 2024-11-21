af0e17a4ad7ee110f81382d9a6076296a295c269
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-11-20 21:30:04 -0330

    Add prop validation to ProtectedRoute
    Add prop validation for type checking

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,15 @@
 import { useAuth } from '@clerk/clerk-react';
 import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function ProtectedRoute({ children}) {
-    const { isSignedIn } = useAuth();
+export default function ProtectedRoute({ children }) {
+  const { isSignedIn } = useAuth();
 
-    if (!isSignedIn ) 
-        return <Navigate to="/" replace />
+  if (!isSignedIn) return <Navigate to="/" replace />;
 
   return children;
 }
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+};
