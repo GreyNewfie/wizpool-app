85592532c72d2ee2047c19607964fdf1c11422bb
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-11-22 17:24:37 -0330

    Update PoolPicksPage state
    Update PoolPicksPage to use the global pool state.

--- src/pages/PoolPicksPage.jsx
+++ src/pages/PoolPicksPage.jsx
@@ -2,33 +2,38 @@ import classes from './PoolPicksPage.module.css';
 import PageHeader from '../components/PageHeader';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import MobileNavMenu from '../components/MobileNavMenu';
-import usePool from '../utils/usePool';
 import DisplayTeams from '../components/DisplayTeams';
 import useIsDesktop from '../utils/useIsDesktop';
 import DesktopNavHeader from '../components/DesktopNavHeader';
 import useStoredPools from '../utils/useStoredPools';
+import { useSelector } from 'react-redux';
 
 export default function PoolPicksPage() {
-  const { pool, createNewPool, changePool } = usePool();
+  const pool = useSelector((state) => state.pool);
   const isDesktop = useIsDesktop();
   const { getNonActivePools } = useStoredPools();
   const nonActivePools = getNonActivePools();
 
   const getTeamsAndPlayers = () => {
-    const clonedPool = pool.clonePool();
-    const teamsWithPlayers = clonedPool.players.flatMap((player) => {
-      return player.teams.map((team) => {
+    const teamsAndPlayersList = pool.players.flatMap((player) => {
+      return player.teams?.map((team) => {
         return {
-          playerName: player.playerName,
+          playerName: player.name,
           team: team,
         };
       });
     });
-    teamsWithPlayers.sort((a, b) => b.team.wins - a.team.wins);
-    return teamsWithPlayers;
+    teamsAndPlayersList.sort((a, b) => b.team.wins - a.team.wins);
+    // Filter out is any falsy values
+    const filteredList = teamsAndPlayersList.filter(Boolean);
+    return filteredList;
   };
 
   const teamsAndPlayersList = getTeamsAndPlayers();
+
+  const changePool = () => {};
+
+  const createNewPool = () => {};
 
   return (
     <div className={classes['page-container']}>
@@ -43,7 +48,7 @@ export default function PoolPicksPage() {
       <div className={classes['pool-picks']}>
         <PageHeader
           headerText="Picked Teams"
-          leftBtnText=<ArrowBackIcon />
+          leftBtnText={<ArrowBackIcon />}
           path="/pool-home"
           poolName={pool.poolName}
           createNewPool={createNewPool}
@@ -54,7 +59,10 @@ export default function PoolPicksPage() {
           <p>See which players have the teams with the most wins</p>
           {teamsAndPlayersList.map((teamAndPlayer, index) => (
             <div key={index} className={classes['player-team-container']}>
-              <DisplayTeams league={pool.league} teams={[teamAndPlayer.team]} />
+              <DisplayTeams
+                league={pool.league}
+                teams={[teamAndPlayer?.team]}
+              />
               <h6>{teamAndPlayer.playerName}</h6>
             </div>
           ))}
