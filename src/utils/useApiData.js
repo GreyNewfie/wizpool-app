8140ed22defbcf2d97c27eda9f22beb6b35879ed
Danny Simms <103758200+GreyNewfie@users.noreply.github.com>
2024-12-26 17:25:59 -0330

    Add authentication to fetching league data
    Add authentication when fetching league data from the backend api.

--- src/utils/useApiData.js
+++ src/utils/useApiData.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { getApiBaseUrl } from '../config/config';
+import { useAuth } from '@clerk/clerk-react';
 
 const BASE_URL = getApiBaseUrl();
 
@@ -7,13 +8,20 @@ export default function useApiData() {
   const [apiData, setApiData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
+  const { getToken } = useAuth();
 
   const fetchData = async (url) => {
     setLoading(true);
     let data;
 
     try {
-      const response = await fetch(url);
+      const token = await getToken();
+      const response = await fetch(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
       console.log('Backend API called from fetchData');
       if (response.status === 200) {
         data = await response.json();
@@ -21,7 +29,7 @@ export default function useApiData() {
         throw new Error(response.statusText);
       }
     } catch (error) {
-      console.log('Error:', error);
+      console.log('Error fetching league data:', error);
       setError(error);
     } finally {
       setLoading(false);
@@ -137,6 +145,7 @@ export default function useApiData() {
     if (!league) {
       throw new Error('League is required to get all teams');
     }
+    
     // Set api url based on league
     const url = `${BASE_URL}/${league}_data`;
 
